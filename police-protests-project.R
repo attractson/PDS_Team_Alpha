c7a21ec62c7ded4cf3965940d990bdc3c5d5f898
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-02-23 21:48:55 -0600

    added basic format for getting tweets that contain some regex expressions and displaying the tibble

--- police-protests-project.R
+++ police-protests-project.R
@@ -3,3 +3,14 @@ library(tidyverse)
 
 # need to make sure tweets file is in folder
 tweets<-read_csv("./Tweets.csv")
+
+str(tweets)
+aTweet<-tweets[1,]$Text # get one row and one column
+split<-str_split(aTweet, pattern = " ")
+str(split)
+str(unlist(split))
+
+
+tweets$Text
+# use something like this to create a list of Trues and Falses to see if each tweet matches some regex expression
+str_detect(tweets$Text, regex("a b c"))
