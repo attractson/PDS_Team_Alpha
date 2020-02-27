1bb428dd8c5984af4799e0a12c3477ffa13d9337
Lunatic <42777805+attractson@users.noreply.github.com>
2020-02-27 11:20:15 -0600

    Mapping
    1

--- group_assigment_0225.R
+++ group_assigment_0225.R
@@ -5,9 +5,16 @@ library(tidyverse)
 #For Danny
 EX<-read_csv("~/Downloads/VOTER_Survey_Jan217_Release1-csv.csv")
 head(EX)
-select(EX, starts_with("inst"))
 #16 variables
-Ex %>%
-  mutate_all(inst_court_2019= na_if(inst_court_2019, inst_court_2019>4)) %>%
+EX1<- EX %>%
+  select(starts_with("inst"))%>%
+  mutate_all(na_if(x, x>4))5 %>%
+  mutate_all()
+  map_int()
+##assign  8
+
+
+
+  
   
   
