eef850591358edc9cde88d75e1f66cc5ec14ccf2
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-05-02 00:44:08 -0500

    data

--- final_project/gettingApprovalData.R
+++ final_project/gettingApprovalData.R
@@ -1,29 +1,32 @@
 library(jsonlite)
 
-state_approval<-c()
-state_
 state_ratings<-fromJSON("C:/Users/ians-pc/Desktop/missouri_approval.json",flatten=TRUE)
 size1<-length(a$trendline)
 size1
 
+entries<-unlist(state_ratings$trendline)
 
-entries<-unlist(a$trendline[c(1:size1)])
-entries
 size2<-length(entries)
 
+times<-substr(names(entries[seq(1,size2,by=3)]),1,13)
+times
+
 approves<-as.vector(entries[seq(1,size2,by=3)])
-approves[size1]
+approves
 
 disapproves<-as.vector(entries[seq(2,size2,by=3)])
-disapproves[size1]
+disapproves
 
 neither<-as.vector(entries[seq(3,size2,by=3)])
-neither[size1]
+neither
+
+ratings_df<-data.frame(time=times,approves,disapproves,neither)
+ratings_df
 
 
 
 
-typeof(a$trendline[1])
+  typeof(a$trendline[1])
 (unlist(a$trendline[1])[1])
 str(as.data.frame(a))
 
