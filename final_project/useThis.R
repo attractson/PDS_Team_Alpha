9390372742feb5048321547d381feb5aac80915e
ianhudson87 <46661041+ianhudson87@users.noreply.github.com>
2020-05-03 23:26:30 -0500

    Create useThis.R

---
+++ final_project/useThis.R
@@ -0,0 +1,100 @@
+rm(list=ls())
+library(jsonlite)
+library(tidyverse)
+
+states<-c("alabama","alaska","arizona","arkansas","california","colorado","connecticut","delaware","florida","georgia","hawaii","idaho","illinois","indiana","iowa","kansas","kentucky","louisiana","maine","montana","nebraska","nevada","new_hampshire","new_jersey","new_mexico","new_york","north_carolina","north_dakota","ohio","oklahoma","oregon","maryland","massachusetts","michigan","minnesota","mississippi","missouri","pennsylvania","rhode_island","south_carolina","south_dakota","tennessee","texas","utah","vermont","virginia","washington","west_virginia","wisconsin","wyoming")
+
+for(state in states){
+  state_ratings<-fromJSON(paste0("./approvalData/",state,".json"),flatten=TRUE)
+  size1<-length(state_ratings$trendline)
+  
+  entries<-unlist(state_ratings$trendline)
+  
+  size2<-length(entries)
+  
+  unix_times<-substr(names(entries[seq(1,size2,by=3)]),1,13)
+  unix_times<-as.numeric(unix_times)/1000
+  as.Date(as.POSIXct(unix_times, origin="1970-01-01"))
+  dates<-as.Date(as.POSIXct(unix_times, origin="1970-01-01"))
+  dates
+  
+  approves<-as.vector(entries[seq(1,size2,by=3)])
+  approves
+  
+  disapproves<-as.vector(entries[seq(2,size2,by=3)])
+  disapproves
+  
+  neither<-as.vector(entries[seq(3,size2,by=3)])
+  neither
+  
+  net_approval<-approves-disapproves
+  
+  state_ratings_df<-data.frame(net_approval)
+  names(state_ratings_df)[1]<-paste0(state,"_net_approval")
+  
+  # append new data to end of running df
+  if(exists("ratings_df")){
+    # df created already
+    new_date<-c(ratings_df$date, dates)
+    new_state<-c(ratings_df$state, c(rep(state, length(dates))))
+    new_net_approval<-c(ratings_df$net_approval, net_approval)
+  }
+  else{
+    # df not created yet
+    new_date<-dates
+    new_state<-rep(state, length(dates))
+    new_net_approval<-net_approval
+  }
+  
+  ratings_df<-data.frame(date=new_date, state=new_state, net_approval=new_net_approval, stringsAsFactors = FALSE)
+}
+head(ratings_df)
+
+typeof(rep(state, length(dates)))
+
+str(ratings_df)
+ratings_by_state<-ratings_df
+
+
+
+
+##############
+
+
+#getting covid data
+library(tidyverse)
+covid_by_county<-read_csv("./us-counties-covid.csv")
+covid_by_state<-covid_by_county %>% group_by(date, state) %>%
+  summarise(cases=sum(cases), deaths=sum(deaths))
+
+covid_by_state
+# change date from char to Date
+covid_by_state<-covid_by_state %>%
+  ungroup(date) %>% ungroup(state) %>%
+  mutate(date=as.Date(date, format="%m/%d/%y")) %>%
+  mutate(state=str_replace(tolower(state), " ", "_"), Diff_date = date - lag(date),  
+         Diff_growth = cases - lag(cases))
+covid_by_state
+
+########################
+
+
+# combining data
+
+# for consistency in data type
+ratings_by_state$date <- as.character(ratings_by_state$date)
+covid_by_state$date <- as.character(covid_by_state$date)
+
+str(ratings_by_state) # ratings dataframe
+str(covid_by_state) # cases and deaths dataframe
+
+combined_data<-inner_join(ratings_by_state, covid_by_state, by=c("state", "date"))
+combined_data
+
+
+# FOR GRAPHING
+
+# example for data from one state
+combined_data[combined_data$state=="alabama",]
+# list of all the states
+states
